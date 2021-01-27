207314c56d36a73197f931c22231171e73861e5e
Adam Judd <adam.judd@rea-group.com.au>
2021-01-27 19:14:48 +1100

    Code cleaning 🧹

--- gdrive_repl.py
+++ gdrive_repl.py
@@ -6,15 +6,28 @@ from click.core import Context
 from click.formatting import HelpFormatter
 from click.exceptions import UsageError
 import copy
+from fs.base import FS
 from fs.tempfs import TempFS
 from fs.subfs import SubFS
 from pydrive.drive import GoogleDrive
 from gdrive_utils import get_drive_instance, get_files_in_drive_dir, add_drive_files_to_sub_fs
-from utils import get_path, get_sub_dir_path
+from utils import get_sub_dir_path
 
 drive: GoogleDrive
-current_dir: SubFS[TempFS]
+current_dir: SubFS[FS]
 drive_ids: Dict[str, str] = {}
+
+def _generate_files(parent_dir: SubFS[FS]) -> None:
+    global drive, drive_ids
+    files = get_files_in_drive_dir(drive, drive_ids[get_sub_dir_path(parent_dir)])
+    drive_ids.update(add_drive_files_to_sub_fs(parent_dir, files))
+
+
+def _parentless(context: Context) -> Context:
+    ctx = copy.deepcopy(context)
+    ctx.parent = None
+    return ctx
+
 
 def start_repl(sdr_dir_path: Path) -> str:
     global drive, current_dir, drive_ids
@@ -24,8 +37,7 @@ def start_repl(sdr_dir_path: Path) -> str:
     gdrive_fs = TempFS(temp_dir=str(temp_dir_path.resolve()))
     current_dir = gdrive_fs.makedir('~')
     drive_ids[current_dir._sub_dir] = 'root'
-    files = get_files_in_drive_dir(drive, drive_ids[get_sub_dir_path(current_dir)])
-    drive_ids.update(add_drive_files_to_sub_fs(current_dir, files))
+    _generate_files(current_dir)
 
     while True:
         user_input = input('> ')
@@ -38,11 +50,6 @@ def start_repl(sdr_dir_path: Path) -> str:
 
         print(gdrive_fs.tree())
 
-
-def _parentless(context: Context) -> Context:
-    ctx = copy.deepcopy(context)
-    ctx.parent = None
-    return ctx
 
 class ReplCommand(click.Command):
     def format_usage(self, ctx: Context, formatter: HelpFormatter) -> None:
@@ -67,18 +74,13 @@ def repl():
 @repl.command(cls=ReplCommand)
 @click.argument('dir', required=True)
 def cd(dir: str) -> SubFS:
-    global drive, current_dir, drive_ids
+    global current_dir
     while not current_dir.exists(dir):
         split_dir = shlex.split(dir)
         next_dir, dir = split_dir[0], split_dir[1:]
         if not current_dir.exists(next_dir):
-            files = get_files_in_drive_dir(drive, drive_ids[get_sub_dir_path(current_dir)])
-            drive_ids.update(add_drive_files_to_sub_fs(current_dir, files))
+            _generate_files(current_dir)
         current_dir = current_dir.opendir(next_dir)
     current_dir = current_dir.opendir(dir)
-    files = get_files_in_drive_dir(drive, drive_ids[get_sub_dir_path(current_dir)])
-    drive_ids.update(add_drive_files_to_sub_fs(current_dir, files))
-    click.echo(dir)
-
-if __name__ == "__main__":
-    start_repl(get_path() / '.sdr/')
+    _generate_files(current_dir)
+    
